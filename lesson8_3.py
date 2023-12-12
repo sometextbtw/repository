e567f985f70e038f083bd254dc2589542e1843db
sometextbtw <amtitkovv@gmail.com>
2023-12-12 22:08:54 +0300

    corrected solution

--- lesson8_3.py
+++ lesson8_3.py
@@ -1,21 +1,31 @@
 import yaml
 
+
 def load(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         return yaml.safe_load(file)
 
+
 def find_tasks(task_name, tasks_data):
-    task_info = next((task for task in tasks_data if task['name'] == task_name), None)
+    task_info = []
+    for task in tasks_data:
+        if task["name"] == task_name:
+            task_info = task
+            break
     dependencies = []
     if task_info:
-        dependencies.extend(task_info.get('dependencies', []))
-        for subtask_name in task_info.get('subtasks', []):
-            dependencies.extend(find_tasks(subtask_name, tasks_data))
+        dependencies.extend(task_info.get("dependencies", []))
     return dependencies
 
+
 def find_build_tasks(build_name, builds_data, tasks_data):
-    build_tasks = next((build['tasks'] for build in builds_data if build['name'] == build_name), [])
+    build_tasks = []
     result_tasks = []
+
+    for build in builds_data:
+        if build["name"] == build_name:
+            build_tasks = build["tasks"]
+            break
 
     for task_name in build_tasks:
         result_tasks.append(task_name)
@@ -23,13 +33,14 @@ def find_build_tasks(build_name, builds_data, tasks_data):
 
     return result_tasks
 
-builds_data = load('builds.yaml')['builds']
-tasks_data = load('tasks.yaml')['tasks']
 
-build_name = 'forward_interest'
+builds_data = load("builds.yaml")["builds"]
+tasks_data = load("tasks.yaml")["tasks"]
+
+build_name = "voice_central"
 build_tasks = find_build_tasks(build_name, builds_data, tasks_data)
 
-print()
+
 print(f"Для {build_name} список задач:")
 for task in build_tasks:
     print(task)
