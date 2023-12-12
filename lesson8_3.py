4e76118563bf98ea05bea1974a800138bd6f7726
sometextbtw <amtitkovv@gmail.com>
2023-12-12 20:18:15 +0300

    asfsaf1

--- lesson8_3.py
+++ lesson8_3.py
@@ -1,41 +1,35 @@
 import yaml
 
-
 def load_yaml(file_path):
-    with open(file_path, "r") as file:
-        data = yaml.safe_load(file)
-    return data
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
+def find_task_dependencies(task_name, tasks_data):
+    task_info = next((task for task in tasks_data if task['name'] == task_name), None)
+    dependencies = []
+    if task_info:
+        dependencies.extend(task_info.get('dependencies', []))
+        for subtask_name in task_info.get('subtasks', []):
+            dependencies.extend(find_task_dependencies(subtask_name, tasks_data))
+    return dependencies
 
-def subtasks(task_name, tasks_data):
-    for task in tasks_data.get("tasks", []):
-        if task["name"] == task_name:
-            return task.get("subtasks", [])
-    return []
+def find_build_tasks(build_name, builds_data, tasks_data):
+    build_tasks = next((build['tasks'] for build in builds_data if build['name'] == build_name), [])
+    result_tasks = []
 
+    for task_name in build_tasks:
+        result_tasks.append(task_name)
+        result_tasks.extend(find_task_dependencies(task_name, tasks_data))
 
-def build_task_list(build_name, builds_data, tasks_data):
-    build_tasks = []
-    for build in builds_data.get("builds", []):
-        if build["name"] == build_name:
-            for task_name in build["tasks"]:
-                build_tasks.append(
-                    {
-                        "name": task_name,
-                        "subtasks": subtasks(task_name, tasks_data),
-                    }
-                )
-    return build_tasks
+    return result_tasks
 
+builds_data = load_yaml('builds.yaml')['builds']
+tasks_data = load_yaml('tasks.yaml')['tasks']
 
-builds_data = load_yaml("builds.yaml")
-tasks_data = load_yaml("tasks.yaml")
+build_name = 'forward_interest'
+build_tasks = find_build_tasks(build_name, builds_data, tasks_data)
 
-target_build_name = "forward_interest"
-
-
-build_tasks_list = build_task_list(target_build_name, builds_data, tasks_data)
-
-for build_task in build_tasks_list:
-    print(f"Задача: {build_task['name']}")
-    print(f"Подзадачи: {', '.join(build_task['subtasks'])}")
+print(f"Билд - {build_name}")
+print("Список задач:")
+for task in build_tasks:
+    print(task)
